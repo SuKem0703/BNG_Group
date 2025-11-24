4cf9b3ab2546dc336c5b1a398fc5256b2276e0d1
unknown <anhnguyen.732004@gmail.com>
2025-11-24 19:20:25 +0700

    MainMenu BG động

---
+++ Assets/!Game/Scripts/Main Menu/InfiniteMover.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SeamlessBackground : MonoBehaviour
+{
+    [Header("Cấu hình")]
+    [Tooltip("Tốc độ trôi (SỐ ÂM để sang trái, ví dụ -100)")]
+    public float speed = -100f;
+
+    [Tooltip("Tổng số lượng Background (Ví dụ 3 cái)")]
+    public int totalBackgrounds = 3;
+
+    private RectTransform rectTransform;
+    private float objectWidth;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        if (rectTransform != null)
+        {
+            objectWidth = rectTransform.rect.width;
+        }
+        else
+        {
+            Debug.LogError("Thiếu RectTransform! Script này chỉ dùng cho UI.");
+        }
+    }
+
+    void Update()
+    {
+        if (rectTransform == null) return;
+
+        rectTransform.anchoredPosition += new Vector2(speed * Time.unscaledDeltaTime, 0);
+
+        if (rectTransform.anchoredPosition.x <= -objectWidth)
+        {
+            RepositionBackground();
+        }
+    }
+
+    void RepositionBackground()
+    {
+        Vector2 offset = new Vector2(objectWidth * totalBackgrounds, 0);
+        rectTransform.anchoredPosition += offset;
+    }
+}
